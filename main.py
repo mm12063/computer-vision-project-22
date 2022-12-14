2fa6dda3e6bf30a542814063e9b60fba87fedd7c
mwj10 <116900388+mwj10@users.noreply.github.com>
2022-12-13 19:55:13 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import pandas as pd
 import file_locs
 import ssl
 
-parser = argparse.ArgumentParser(description="Main Arguments")
+parser = argparse.ArgumentParser(description="DESCRIPTION")
 parser.add_argument('--batch-size', type=int, default=2, metavar='N',
                     help='input batch size for training (default: 2)')
 parser.add_argument('--epochs', type=int, default=10, metavar='N',
@@ -25,8 +25,10 @@ parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                     help='number of intervals to log (default: 10)')
 parser.add_argument('--num-workers', type=int, default=0, metavar='N',
                     help='number of workers to use (default: 0)')
-parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                    help='learning rate (default: 0.01)')
+parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                    help='learning rate (default: 1.0)')
+parser.add_argument('--momentum', type=float, default=0.9, metavar='momentum',
+                        help='Change the Momentum of Optimizer (default: 0.9)')
 parser.add_argument('--model-name', type=str, default="resent18", metavar='model',
                     help='Resnet Size: resent18, resnet50, resnet152')
 parser.add_argument('--print-vals', action='store_true', default=False,
@@ -43,14 +45,15 @@ parser.add_argument('--val-ds-sz', type=int, default=640, metavar='N',
                     help='Validation Dataset Size (default: 640)')
 parser.add_argument('--save-model', action="store_true", default=False,
                     help='Saving Model')
-parser.add_argument('--save-model-dir', type=str, default="./", metavar='model_dir',
+parser.add_argument('--save-model-dir', type=str, default=".", metavar='model_dir',
                     help='Directory to Save Model')
 parser.add_argument('--save-plots', action="store_true", default=False,
                     help='Saving Plots')
-parser.add_argument('--save-plot-dir', type=str, default="./",  metavar='plot_dir',
+parser.add_argument('--save-plot-dir', type=str, metavar='plot_dir',
                     help='Directory to Save Plots')
 parser.add_argument('--root-dir', type=str, default="./", metavar='root_dir',
-                    help='root dir of the project')
+                        help='root dir of the project')
+
 args = parser.parse_args()
 
 NUM_CLASSES = 45
@@ -59,20 +62,26 @@ ssl._create_default_https_context = ssl._create_unverified_context
 # Reproducibility
 torch.manual_seed(42)
 
-
 class McModel(nn.Module):
     def __init__(self):
         super(McModel, self).__init__()
-
+        
         if args.model_name == "resnet50":
             self.network = models.resnet50(weights='ResNet50_Weights.DEFAULT')
         elif args.model_name == "resnet152":
             self.network = models.resnet152(weights='ResNet152_Weights.DEFAULT')
+        elif args.model_name == "alexnet":
+            self.network = models.alexnet(weights='AlexNet_Weights.DEFAULT')
+        elif args.model_name == "vgg19":
+            self.network = models.vgg19(weights='VGG19_Weights.DEFAULT')
+            in_feats = self.network.classifier[6].in_features;
+            self.network.classifier[6] = nn.Linear(in_feats,NUM_CLASSES)
         else:
             self.network = models.resnet18(weights='ResNet18_Weights.DEFAULT')
-
-        self.network.fc = nn.Linear(self.network.fc.in_features, NUM_CLASSES)
-
+            
+        if not args.model_name == "vgg19":
+            self.network.fc = nn.Linear(self.network.fc.in_features, NUM_CLASSES)
+        
     def forward(self, x):
         output = self.network(x)
         m = nn.Sigmoid()
@@ -80,34 +89,31 @@ class McModel(nn.Module):
 
 
 # Set the Loss/Optimizers
-model = McModel()
+model = McModel();
 criterion = nn.BCELoss()
 optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
+#TODO
+# Balance
 
-def plot(metric, train_vals, test_vals, xtick_interval=2, save=False, save_loc=None, plot_name="Default_Name"):
-    plt.figure(figsize=(5, 3))
+def plot(metric, train_vals, test_vals, xtick_interval=2, save=False, save_loc="./", plot_name="No Name Given"):
+    figure(figsize=(5, 3))
     plt.plot(train_vals, color="red")
     plt.plot(test_vals, color="blue")
     plt.xlabel('epoch')
     plt.ylabel(metric)
-    plt.legend(['Train', 'Test'])
+    plt.legend(['Train','Test'])
     plt.title(f'Train vs Test {metric.capitalize()}')
-    plt.xticks(np.arange(0, len(train_vals) + 1, xtick_interval))
-    if save:
-        if not os.path.exists(save_loc):
-            os.makedirs(save_loc)
-        full_loc = f"{save_loc}{plot_name}"
-        print(f"Plot saved to: {full_loc}")
-        plt.savefig(full_loc)
+    plt.xticks(np.arange(0, len(train_vals)+1, xtick_interval))
     plt.show()
+    if save:
+        plt.savefig(save_loc)
 
 
 def get_mean_and_std(train_dataset):
     imgs = torch.stack([img_t for img_t, _ in train_dataset], dim=3)
     print(imgs.view(3, -1).mean(dim=1))
     print(imgs.view(3, -1).std(dim=1))
-
 
 class CustomDataSet(Dataset):
     def __init__(self, main_dir, y_values, transform):
@@ -126,8 +132,12 @@ class CustomDataSet(Dataset):
         tensor_image = self.transform(image)
         return tensor_image, self.y[idx]
 
-
 def imshow(img, title=None):
+    # img = img / 2 + 0.5
+    # npimg = img.numpy()
+    # plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    # plt.show()
+
     remove_norm = T.Normalize(
         mean=[-0.5130 / 0.2551, -0.3182 / 0.1793, -0.1666 / 0.1329],
         std=[1 / 0.2551, 1 / 0.1793, 1 / 0.1329]
@@ -135,6 +145,11 @@ def imshow(img, title=None):
 
     img = remove_norm(img)
     inp = img.numpy().transpose((1, 2, 0))
+    # mean = np.array([0.485, 0.456, 0.406])
+    # std = np.array([0.229, 0.224, 0.225])
+    # inp = std * inp + mean
+    # inp = np.clip(inp, 0, 1)
+
     plt.imshow(inp)
     if title is not None:
         plt.title(title)
@@ -151,7 +166,7 @@ def train(args, epoch, model, optimizer, criterion, train_loader):
         optimizer.zero_grad()
         output = model(data)
         if args.print_vals:
-            print("output.shape", output.shape)
+            print("output.shape",output.shape)
             print("target.shape", targets.shape)
 
         targets = targets.to(torch.float32)
@@ -166,65 +181,65 @@ def train(args, epoch, model, optimizer, criterion, train_loader):
 
         if predicted.equal(targets):
             correct += 1
-
+        
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                       100. * batch_idx / len(train_loader), loss.item()))
+                100. * batch_idx / len(train_loader), loss.item()))
 
-    train_acc = 100. * correct / total
+    train_acc = 100. * correct/total
     train_loss /= len(train_loader.dataset)
+    print("train_loss",train_loss)
 
     return train_loss, train_acc
-
 
 def validation(model, val_loader):
     model.eval()
     validation_loss = 0
     correct = 0
-    partial_cor = 0
+    partial_cor = 0;
     class_cor = torch.zeros(NUM_CLASSES)
     for data, targets in val_loader:
         output = model(data)
         targets = targets.to(torch.float32)
-        validation_loss += criterion(output, targets)
-
+        validation_loss+= criterion(output, targets)
+        
         predicted = output
         predicted = (predicted > 0.5).float()
-
+        
         for i in range(len(targets)):
-            t = targets[i]
-            p = predicted[i]
-            res_by_class = p.eq(t)
-
+            t = targets[i];
+            p = predicted[i];
+            res_by_class = p.eq(t);
+            
             if p.equal(t):
                 correct += 1
             if torch.any(res_by_class):
-                partial_cor += 1
-
+                partial_cor +=1
+            
             for j in range(len(class_cor)):
                 if res_by_class[j]:
-                    class_cor[j] += 1
-
+                    class_cor[j]+=1;
+            
     validation_loss /= len(val_loader.dataset)
     acc = 100. * correct / len(val_loader.dataset)
     pacc = 100 * partial_cor / len(val_loader.dataset)
     num_items = len(val_loader.dataset)
-    print(
-        '\nValidation set:\n Average loss: {:.4f},\n Partial Accuracy: {}/{} ({:.2f}%)\n Full Accuracy: {}/{} ({:.2f}%)'.format(
-            validation_loss,
-            partial_cor, num_items, pacc,
-            correct, num_items, acc))
-    print(' class_cor: ' + str(class_cor))
-    print(' class_acc: ' + str(class_cor / num_items * 100))
-    return validation_loss.item(), acc
+    print('\nValidation set:\n Average loss: {:.4f},\n Partial Accuracy: {}/{} ({:.2f}%)\n Full Accuracy: {}/{} ({:.2f}%)'.format(
+        validation_loss,
+        partial_cor,num_items, pacc,
+        correct,num_items,acc))
+    print(' class_cor: '+str(class_cor));
+    print(' class_acc: '+str(class_cor/num_items*100));
 
+    return validation_loss, acc
 
 def main():
+    
     # Create the transformations
     trans = []
     trans.append(T.ToTensor())
-    trans.append(T.Resize((args.image_size, args.image_size)))
+    #trans.append(T.Resize((args.image_size, args.image_size)))
     trans.append(T.Normalize(
         (0.5130, 0.3182, 0.1666),
         (0.2551, 0.1793, 0.1329))
@@ -235,17 +250,22 @@ def main():
         trans.append(T.RandomErasing(p=0.6, scale=(0.04, 0.12)))
         is_cutout_used = "True"
 
-    # Set the transformations
     transforms = T.Compose(trans)
 
     DROP_VALS = ['ID', 'Disease_Risk']
-    y_train_vals = pd.read_csv(args.root_dir + file_locs.TRAIN_CSV)
+    
+    #y_train_vals = pd.read_csv(args.root_dir + file_locs.TRAIN_CSV)
+    y_train_vals = pd.read_csv(args.root_dir +file_locs.TRAIN_CSV)
     y_train_vals = np.array(y_train_vals.drop(DROP_VALS, axis=1))
     y_val_vals = pd.read_csv(args.root_dir + file_locs.VAL_CSV)
     y_val_vals = np.array(y_val_vals.drop(DROP_VALS, axis=1))
 
-    train_ds_location = args.root_dir + file_locs.TRAIN_DS + "resized/auto_crop/same_size/"
-    val_ds_location = args.root_dir + file_locs.VAL_DS + "resized/auto_crop/same_size/"
+    #train_ds_location = args.root_dir + file_locs.TRAIN_DS + "resized/auto_crop/same_size/"
+    #train_ds_location = "../../../../scratch/mm12063/cv_project_22/data/fundus_ds/Training_Set/Training_Set/Training/resized_complete/64/"
+    train_ds_location = "../../../../scratch/mm12063/cv_project_22/data/fundus_ds/Training_Set/Training_Set/Training/resized_complete/upsampled_64/"
+    #val_ds_location = args.root_dir + file_locs.VAL_DS + "resized/auto_crop/same_size/"
+    #val_ds_location = "../../../../scratch/mm12063/cv_project_22/data/fundus_ds/fake_val_256/"
+    val_ds_location = "../../../../scratch/mm12063/cv_project_22/data/fundus_ds/Evaluation_Set/Evaluation_Set/Validation/resized_complete/64/"
     train_dataset = CustomDataSet(train_ds_location, y_values=y_train_vals, transform=transforms)
     val_dataset = CustomDataSet(val_ds_location, y_values=y_val_vals, transform=transforms)
     # get_mean_and_std(train_dataset)
@@ -262,8 +282,8 @@ def main():
         val_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
 
     # Show some images
-    # images, labels = next(iter(train_loader))
-    # imshow(utils.make_grid(images))
+    images, labels = next(iter(train_loader))
+    imshow(utils.make_grid(images))
 
     losses_train = []
     losses_test = []
@@ -287,10 +307,8 @@ def main():
     full_model_name += f'__e_{args.epochs}__c_{is_cutout_used}'
 
     if args.save_model:
-        if not os.path.exists(args.save_model_dir):
-            os.makedirs(args.save_model_dir)
         full_path = args.save_model_dir + full_model_name + ".pth"
-        print(f"model: {model}")
+        print("model ?: "+str(model));
         torch.save(model.state_dict(), full_path)
         print(f'Saved model to {full_path}')
 
@@ -302,7 +320,7 @@ def main():
          xtick_interval=xticks,
          save=args.save_plots,
          save_loc=args.save_plot_dir,
-         plot_name=f'loss__{full_model_name}.png'
+         plot_name= f'loss_{full_model_name}.png'
          )
     plot('accuracy',
          acc_train,
@@ -310,8 +328,99 @@ def main():
          xtick_interval=xticks,
          save=args.save_plots,
          save_loc=args.save_plot_dir,
-         plot_name=f'acc__{full_model_name}.png'
+         plot_name = f'acc_{full_model_name}.png'
          )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
